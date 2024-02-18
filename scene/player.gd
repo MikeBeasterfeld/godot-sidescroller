db3684beadd55b533df6b581709b27f8bdc1e026
Mike Beasterfeld <mike.beasterfeld@gmail.com>
2024-02-18 14:42:20 -0600

    Stop jump when jump button not pressed

--- scene/player.gd
+++ scene/player.gd
@@ -12,9 +12,12 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
+	if not Input.is_action_pressed("ui_accept") and not is_on_floor() and velocity.y < 0:
+		velocity.y = 0
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
